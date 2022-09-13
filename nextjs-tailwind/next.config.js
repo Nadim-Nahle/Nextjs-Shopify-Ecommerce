b3a731df97b1361e1077895913d60594fa66dbb0
Nadim <nahlenadim@gmail.com>
2022-09-13 12:42:07 +0300

    Frontend-Nextjs Create next-config.js

--- nextjs-tailwind/next.config.js
+++ nextjs-tailwind/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+    SHOPIFY_STOREFRONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN
+  },
+  images: {
+    domains: ['cdn.shopify.com']
+  }
 
-module.exports = nextConfig
+}
