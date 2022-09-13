84e5a95467c007c667eee1a7d628df72678607a6
Nadim <nahlenadim@gmail.com>
2022-09-13 20:18:21 +0300

    Frontend-Nextjs Create product card component

--- nextjs-tailwind/components/ProductCard.jsx
+++ nextjs-tailwind/components/ProductCard.jsx
@@ -1,5 +1,28 @@
-const ProductCard = () => {
-  return <div>ProductCard</div>;
+import Link from "next/link";
+import Image from "next/image";
+
+const ProductCard = ({ product }) => {
+  const { handle, title } = product.node;
+
+  const { altText, originalSrc } = product.node.images.edges[0].node;
+
+  return (
+    <Link href={`/products/${handle}`}>
+      <a className="group">
+        <div className="w-full bg-gray-200 rounded-3xl overflow-hidden">
+          <div className="relative group-hover:opacity-75 h-72">
+            <Image
+              src={originalSrc}
+              alt={altText}
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+        </div>
+        <h3 className="mt-4 text-lg font-medium text-gray-900">{title}</h3>
+      </a>
+    </Link>
+  );
 };
 
 export default ProductCard;
