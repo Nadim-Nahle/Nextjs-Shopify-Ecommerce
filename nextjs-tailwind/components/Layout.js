988f41278f85dc278eeb4fc1c1524d0f10dc95a9
Nadim <nahlenadim@gmail.com>
2022-09-14 07:46:24 +0300

    Frontend-Nextjs IMPORT Nav  component

--- nextjs-tailwind/components/Layout.js
+++ nextjs-tailwind/components/Layout.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Nav from './Nav'
 
 export default function Layout({ children }) {
     return (
         <div className='flex flex-col justify-between min-h-screen'>
+            <Nav />
             <main>
                 {children}
             </main>
